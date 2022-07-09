b9d979755d09342574811e012f6f12cfb153f327
mostofa kamal <mostofakamal1990@gmail.com>
2022-07-09 11:27:29 +0600

    AppStateContext context api use

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
+
+import { AppStateProvider } from "./components/AppStateContext"
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <AppStateProvider>
     <App />
+    </AppStateProvider>
+  
   </React.StrictMode>
 );
 
